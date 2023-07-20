91461916f617cb4e1b4d3872edf239008270a593
algamawiy <aliyuassalafiygama@gmail.com>
2023-07-20 10:49:42 -0700

    Modified: 3-last_digit.py file

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -7,8 +7,9 @@ number = int(number)
 last_digit = int(last_digit)
 if number > 0 and last_digit > 5:
     print(f"Last digit of {number} is {last_digit} and is greater than 5")
-if int(last_digit) == 0:
+if last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
-if int(last_digit) < 6 and int(last_digit) != 0:
+if last_digit < 6 and last_digit != 0:
     print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+    
     

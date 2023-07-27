f53feb27c07ae3c8f290c83e88fd1339cb16c85a
algamawiy <aliyuassalafiygama@gmail.com>
2023-07-27 05:54:19 -0700

    Modified: 5-print_comb2.py file

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!usr/bin/python3
 def print_numbers():
     for i in range(100):
-        print("{:02}".format(i), end=", ")
+        print("{:02}".format(i), end=", " if i<99 else "\n")
     print("\n")
 
 if __name__ == "__main__":
